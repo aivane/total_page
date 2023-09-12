e41a54046f4acf07739c283d09dc419a29bb2eea
aivane <Thannapat.Kham@mail.kmutt.ac.th>
2023-09-12 07:06:54 +0700

    9/12/2003

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,19 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if (arrayItems === null || arrayItems === undefined) {
+    return undefined
+  }
+  if (rowsPerPage === null||rowsPerPage === undefined||rowsPerPage === 0) {
+    return 1
+  }
+  arrayItems = arrayItems.length
+  if (arrayItems % rowsPerPage !== 0) {
+    return Math.ceil(arrayItems/rowsPerPage)
+  }
+  if (arrayItems % rowsPerPage === 0) {
+    return arrayItems/rowsPerPage
+  }
 }
+console.log(totalPages(null,20))
 module.exports = totalPages
